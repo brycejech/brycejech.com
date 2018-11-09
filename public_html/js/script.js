7fc4a2372530c08ed6a441d6d0b8ad996fac5e6f
Bryce Jech <bryce@brycejech.com>
2018-11-09 14:06:39 -0600

    activate nav item for corresponding visible section

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -2,17 +2,46 @@
 
 (function(){
 
-    function navClick(event){
-        document.querySelector('nav').querySelectorAll('li')
-            .forEach(function(elem){
+    var activateNav = (function(){
+        // cache navigation elements
+        var elems = [].slice.call(document.querySelector('nav').querySelectorAll('li'));
+
+        return function activateNav(el){
+            elems.forEach(function(elem){
                 elem.classList.remove('active');
             });
-        this.parentNode.classList.add('active');
-    }
+
+            el.parentNode.classList.add('active');
+        }
+
+    })();
 
     document.querySelector('nav').querySelectorAll('a')
         .forEach(function(elem){
-            elem.addEventListener('click', navClick);
+            elem.addEventListener('click', function(){ activateNav(this) });
         });
+
+
+    // Activate nav item for corresponding section
+    var sections = [
+        '#about',
+        '#projects',
+        '#talks',
+        '#contact'
+    ]
+
+    setInterval(function(){
+        for(var i = 0, len = sections.length; i < len; i++){
+            var section = sections[i],
+                bounds  = document.querySelector(section)
+                            .getBoundingClientRect();
+
+            if(bounds.top <= 0 && bounds.bottom > 0){
+                var el = document.querySelector(`[href="${ section }"]`);
+                activateNav(el);
+                break;
+            }
+        }
+    }, 500);
 
 })();
